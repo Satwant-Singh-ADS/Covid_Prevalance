285ef6e128a92a83e46af3d63f984afab7aa0058
Satwant-Singh-ADS <89945989+Satwant-Singh-ADS@users.noreply.github.com>
2023-02-04 17:33:06 -0800

    Prevalnce code added

--- CDC_Sero.py
+++ CDC_Sero.py
@@ -26,7 +26,6 @@ def hampel_filter_forloop(input_series, window_size=15, n_sigmas=4):
     
     return new_series, indices
 
-data_4 = pd.read_csv("matlab_python_poc/Covid_Prevalance/data_4.csv",header=None)
 
 def Smooth_epiData(data_4,smooth_factor = 14,week_correction = 1,week_smoothing = 1):
     
@@ -129,154 +128,175 @@ def Smooth_epiData(data_4,smooth_factor = 14,week_correction = 1,week_smoothing 
 
 
 
-import pandas as pd
-data_4 = pd.read_csv("matlab_python_poc/Covid_Prevalance/data_4.csv",header=None)
 
-import requests
-import pandas as pd
-from datetime import datetime, timedelta
-import numpy as np
+def CDC_SERO_Function():
+    import pandas as pd
+
+    import requests
+    import pandas as pd
+    from datetime import datetime, timedelta
+    import numpy as np
+
 # Load sero data from CDC
-url = 'https://data.cdc.gov/api/views/mtc3-kq6r/rows.csv?accessType=DOWNLOAD'
-response = requests.get(url)
-open('dummy.csv', 'wb').write(response.content)
-sero_data = pd.read_csv('dummy.csv')
+    url = 'https://data.cdc.gov/api/views/mtc3-kq6r/rows.csv?accessType=DOWNLOAD'
+    response = requests.get(url)
+    open('dummy.csv', 'wb').write(response.content)
+    sero_data = pd.read_csv('dummy.csv')
+
+    import pickle
+
+    filehandler = open("data_4.pkl", 'rb') 
+    #     print(a)
+    data_4 = pickle.load(filehandler)
 
 
-sero_data['Region Abbreviation_tmp'] = sero_data['Region Abbreviation'].str[:2]
+    sero_data['Region Abbreviation_tmp'] = sero_data['Region Abbreviation'].str[:2]
 
-sero_data['x1_tmp'] = sero_data["Lower CI %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
+    sero_data['x1_tmp'] = sero_data["Lower CI %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
 
-sero_data['x2_tmp'] = sero_data["Rate %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
+    sero_data['x2_tmp'] = sero_data["Rate %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
 
-sero_data['x3_tmp'] = sero_data["Upper CI  %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
+    sero_data['x3_tmp'] = sero_data["Upper CI  %[Total Prevalence]"]*sero_data["n [Total Prevalence]"]
 
-tmp = sero_data.groupby(['Region Abbreviation_tmp','Median \nDonation Date']).agg({"n [Total Prevalence]":"sum",
-                                                   }).reset_index()
+    tmp = sero_data.groupby(['Region Abbreviation_tmp','Median \nDonation Date']).agg({"n [Total Prevalence]":"sum",
+                                                       }).reset_index()
 
-sero_subset = sero_data[['Region Abbreviation_tmp','Median \nDonation Date','x1_tmp','x2_tmp','x3_tmp']]
+    sero_subset = sero_data[['Region Abbreviation_tmp','Median \nDonation Date','x1_tmp','x2_tmp','x3_tmp']]
 
-# sero_subset['key'] = sero_subset['Region Abbreviation_tmp']+"_"+sero_subset['Median \nDonation Date']
+    # sero_subset['key'] = sero_subset['Region Abbreviation_tmp']+"_"+sero_subset['Median \nDonation Date']
 
-new_df = pd.merge(sero_subset, tmp,  how='left', left_on=['Region Abbreviation_tmp','Median \nDonation Date'],\
-                  right_on = ['Region Abbreviation_tmp','Median \nDonation Date'])
+    new_df = pd.merge(sero_subset, tmp,  how='left', left_on=['Region Abbreviation_tmp','Median \nDonation Date'],\
+                      right_on = ['Region Abbreviation_tmp','Median \nDonation Date'])
 
-new_df['x1'] = (new_df['x1_tmp']/new_df['n [Total Prevalence]'])*0.01
-new_df['x2'] = (new_df['x2_tmp']/new_df['n [Total Prevalence]'])*0.01
-new_df['x3'] = (new_df['x3_tmp']/new_df['n [Total Prevalence]'])*0.01
-
-
-sero_data_processed = new_df.groupby(['Region Abbreviation_tmp','Median \nDonation Date']).agg({"x1":"sum",\
-                                                                                                "x2":"sum",\
-                                                                                                "x3":"sum",\
-                                                   }).reset_index()
+    new_df['x1'] = (new_df['x1_tmp']/new_df['n [Total Prevalence]'])*0.01
+    new_df['x2'] = (new_df['x2_tmp']/new_df['n [Total Prevalence]'])*0.01
+    new_df['x3'] = (new_df['x3_tmp']/new_df['n [Total Prevalence]'])*0.01
 
 
-abvs = pd.read_csv('us_states_abbr_list.txt', header=None)
-
-state_map = dict(zip(abvs[0], range(len(abvs))))
-try:
-    xx = sero_data_processed['Median \nDonation Date']
-except:
-    xx = sero_data_processed['MedianDonationDate']
-whichday = [0]*len(xx)
-
-for ii in range(len(xx)):
-    whichday[ii] = (datetime.strptime(xx[ii], '%m/%d/%Y') - datetime(2020, 1, 23)).days
+    sero_data_processed = new_df.groupby(['Region Abbreviation_tmp','Median \nDonation Date']).agg({"x1":"sum",\
+                                                                                                    "x2":"sum",\
+                                                                                                    "x3":"sum",\
+                                                       }).reset_index()
 
 
+    abvs = pd.read_csv('us_states_abbr_list.txt', header=None)
 
-un_ts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
-un_lts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
-un_uts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
+    state_map = dict(zip(abvs[0], range(len(abvs))))
+    try:
+        xx = sero_data_processed['Median \nDonation Date']
+    except:
+        xx = sero_data_processed['MedianDonationDate']
+    whichday = [0]*len(xx)
 
-sero_data_processed['idx'] = sero_data_processed['Median \nDonation Date'].apply(lambda x :\
-                                    (datetime.strptime(x, '%m/%d/%Y') - datetime(2020, 1, 23)).days
-                                                                                )
-
-
-sero_data_processed['cidx'] = sero_data_processed['Region Abbreviation_tmp'].apply(lambda x : \
-                                                   state_map.get(x,None))
-
-popu_tmp = pd.read_csv("matlab_python_poc/Covid_Prevalance/us_states_population_data.txt",header=None)
-
-popu = popu_tmp[0].to_list()
-
-for cidx in range(data_4.shape[0]):
-    for ii in range(data_4.shape[1]):
-#         print(cidx)
-        if len(sero_data_processed[sero_data_processed['cidx'] == cidx])>0:
-            if len(sero_data_processed[sero_data_processed['idx'] == ii])>0:
-                data_subset = sero_data_processed[(sero_data_processed['cidx']==cidx) & (sero_data_processed['idx']==ii)]
-                if len(data_subset)>0:
-                    x1 = data_subset['x1'].values[0]
-                    x2 = data_subset['x2'].values[0]
-                    x3 = data_subset['x3'].values[0]                
-    #                     print(x1*popu[cidx])
-    #                 x2 = 
-#                     try:
-#                         data_4[cidx, ii]
-#                         print("aaa",cidx,ii)
-                        
-#                     except:
-#                         print(cidx,ii)
-#                         break
-                    try:
-                        un_lts[cidx, (ii)] = x1*popu[cidx]/data_4.iloc[cidx, ii]
-                        un_ts[cidx, (ii)] = x2*popu[cidx]/data_4.iloc[cidx, ii]
-                        un_uts[cidx, (ii)] = x3*popu[cidx]/data_4.iloc[cidx, ii]
-                    except:
-                        continue
-                
-        
-        
-
-
-import numpy as np
-
-thisday = data_4.shape[1]
-
-un_ts[un_ts<1] = np.nan
-un_ts_s = pd.DataFrame(un_ts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
-
-
-un_lts[un_lts<1] = np.nan
-un_lts_s = pd.DataFrame(un_lts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
-
-un_uts[un_uts<1] = np.nan
-un_uts_s = pd.DataFrame(un_uts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
-
-
-dd_s = Smooth_epiData(data_4)
-ddata = np.diff(dd_s)
-
-
-temp = pd.DataFrame(ddata)
-temp['new'] = 0 ### add a constant colum with value 0 
-
-ddata = temp[[list(temp.columns)[-1]]+list(temp.columns)[0:-1]]
+    for ii in range(len(xx)):
+        whichday[ii] = (datetime.strptime(xx[ii], '%m/%d/%Y') - datetime(2020, 1, 23)).days
 
 
 
-true_new_infec = [
-    pd.DataFrame(np.multiply(un_lts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean(),
-    pd.DataFrame(np.multiply(un_ts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean(),
-    pd.DataFrame(np.multiply(un_uts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean()
-]
+    un_ts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
+    un_lts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
+    un_uts = np.empty((data_4.shape[0],data_4.shape[1]))*np.nan
+
+    sero_data_processed['idx'] = sero_data_processed['Median \nDonation Date'].apply(lambda x :\
+                                        (datetime.strptime(x, '%m/%d/%Y') - datetime(2020, 1, 23)).days
+                                                                                    )
+
+
+    sero_data_processed['cidx'] = sero_data_processed['Region Abbreviation_tmp'].apply(lambda x : \
+                                                       state_map.get(x,None))
+
+    popu_tmp = pd.read_csv("us_states_population_data.txt",header=None)
+
+    popu = popu_tmp[0].to_list()
+
+    for cidx in range(data_4.shape[0]):
+        for ii in range(data_4.shape[1]):
+    #         print(cidx)
+            if len(sero_data_processed[sero_data_processed['cidx'] == cidx])>0:
+                if len(sero_data_processed[sero_data_processed['idx'] == ii])>0:
+                    data_subset = sero_data_processed[(sero_data_processed['cidx']==cidx) & (sero_data_processed['idx']==ii)]
+                    if len(data_subset)>0:
+                        x1 = data_subset['x1'].values[0]
+                        x2 = data_subset['x2'].values[0]
+                        x3 = data_subset['x3'].values[0]                
+        #                     print(x1*popu[cidx])
+        #                 x2 = 
+    #                     try:
+    #                         data_4[cidx, ii]
+    #                         print("aaa",cidx,ii)
+
+    #                     except:
+    #                         print(cidx,ii)
+    #                         break
+                        try:
+                            un_lts[cidx, (ii)] = x1*popu[cidx]/data_4.iloc[cidx, ii]
+                            un_ts[cidx, (ii)] = x2*popu[cidx]/data_4.iloc[cidx, ii]
+                            un_uts[cidx, (ii)] = x3*popu[cidx]/data_4.iloc[cidx, ii]
+                        except:
+                            continue
 
 
 
-un_array = [
-    np.sum(np.multiply(un_lts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1],
-    np.sum(np.multiply(un_ts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1],
-    np.sum(np.multiply(un_uts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1]
-]
 
-un_array[0][np.isnan(un_array[0])] = 1
 
-un_array[1][np.isnan(un_array[1])] = 1
+    import numpy as np
 
-un_array[2][np.isnan(un_array[2])] = 1
+    thisday = data_4.shape[1]
+
+    un_ts[un_ts<1] = np.nan
+    un_ts_s = pd.DataFrame(un_ts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
+
+
+    un_lts[un_lts<1] = np.nan
+    un_lts_s = pd.DataFrame(un_lts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
+
+    un_uts[un_uts<1] = np.nan
+    un_uts_s = pd.DataFrame(un_uts).interpolate(axis=1,limit_direction='both').fillna(1).to_numpy()
+
+
+    dd_s = Smooth_epiData(data_4)
+    ddata = np.diff(dd_s)
+
+
+    temp = pd.DataFrame(ddata)
+    temp['new'] = 0 ### add a constant colum with value 0 
+
+    ddata = temp[[list(temp.columns)[-1]]+list(temp.columns)[0:-1]]
+
+
+
+    true_new_infec = [
+        pd.DataFrame(np.multiply(un_lts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean(),
+        pd.DataFrame(np.multiply(un_ts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean(),
+        pd.DataFrame(np.multiply(un_uts_s,ddata.to_numpy())).rolling(window=28,axis=1,min_periods=0).mean()
+    ]
+
+
+
+    un_array = [
+        np.sum(np.multiply(un_lts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1],
+        np.sum(np.multiply(un_ts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1],
+        np.sum(np.multiply(un_uts_s,ddata.to_numpy()),axis=1)/dd_s[:,-1]
+    ]
+
+    un_array[0][np.isnan(un_array[0])] = 1
+
+    un_array[1][np.isnan(un_array[1])] = 1
+
+    un_array[2][np.isnan(un_array[2])] = 1
+
+
+    import pickle 
+    with open("true_new_infec.pkl", "wb") as f:
+        pickle.dump(true_new_infec, f)
+    with open("un_array.pkl", "wb") as f:
+        pickle.dump(un_array, f)
+#     print(un_array)
+    
+
+#     filehandler = open("un_array.pkl", 'wb') 
+#     #     print(a)
+#     pickle.dump(un_array, filehandler)
 
 
 
--- prevalence_ww.py
+++ prevalence_ww.py
@@ -2,42 +2,85 @@ import pandas as pd
 import requests
 import datetime
 
+from smooth_epidata import *
+
 sel_url = 'https://raw.githubusercontent.com/biobotanalytics/covid19-wastewater-data/master/wastewater_by_county.csv'
 ww_data = pd.read_csv(sel_url)
 
 abvs = pd.read_csv('us_states_abbr_list.txt', header=None)[0].to_list()
-fips_tab = pd.read_csv('reich_fips.txt', names = ["location", "population"])
 
-fips_tab.location = fips_tab.location.replace({'US':'0'})
-fips_idx = fips_tab.location.isin(ww_data.fipscode).nonzero()[0]
-xx = ww_data.sampling_week
+
+fips_tab = pd.read_csv('reich_fips.txt')
+
+
+def impute_us(x):
+    if x=='US':
+        return '0'
+    else:
+        return x
+fips_tab['location'] = fips_tab['location'].apply(impute_us)
+
+ww_data['fipscode'] = ww_data.fipscode.astype(str)
+
+ww_data_fips = ww_data.fipscode.to_list()
+
+fips_tab_codes = fips_tab['location'].to_list()
+
+fips_idx = []
+second_grp = [float(k) for k in (fips_tab_codes)]
+for w in range(len(ww_data_fips)):
+    if float(ww_data_fips[w]) in second_grp:
+        
+        fips_idx.append(second_grp.index(float(ww_data_fips[w])))
+    else:
+        fips_idx.append(0)
+        
+
+import numpy as np
+from datetime import datetime, timedelta
+xx = ww_data.sampling_week.to_list()
 
 whichday = np.zeros(len(xx))
 for ii in range(len(xx)):
-    whichday[ii] = (xx[ii] - datetime.datetime(2020, 1, 23)).days
+    whichday[ii] = int((datetime.strptime(xx[ii], '%Y-%m-%d') - datetime(2020, 1, 23)).days)
 
-ww_ts = np.zeros((len(data_4)))
-ww_pres = np.zeros((len(data_4)))
+
+from latest_us_data import *
+get_data()
+filehandler = open("data_4.pkl", 'rb') 
+#     print(a)
+data_4 = pickle.load(filehandler)
+
+ww_ts = np.zeros((data_4.shape[0],data_4.shape[1]))
+
+ww_pres = np.zeros((data_4.shape[0],data_4.shape[1]))
 
 abvs_idx = [abvs.index(i) for i in ww_data.state if i in abvs]
+
+popu = np.loadtxt('us_states_population_data.txt')
 
 for ii in range(len(xx)):
     if whichday[ii] < 1 or fips_idx[ii] < 1:
         continue
-    ww_pres[abvs_idx[ii], whichday[ii]] = 1
-    ww_ts[abvs_idx[ii], whichday[ii]] = ww_ts[abvs_idx[ii], whichday[ii]] + ww_data.effective_concentration_rolling_average[ii]*fips_tab.population[fips_idx[ii]]/popu[abvs_idx[ii]]
+#     print(ii)
+    ww_pres[abvs_idx[ii], int(whichday[ii])] = 1
+    ww_ts[abvs_idx[ii], int(whichday[ii])] = ww_ts[abvs_idx[ii], int(whichday[ii])] + ww_data.effective_concentration_rolling_average[ii]*fips_tab.population[fips_idx[ii]]/popu[abvs_idx[ii]]
+
 
 ww_ts[ww_pres < 1] = np.nan
 ww_ts[ww_ts <= 0] = np.nan
 
-ww_ts1 = pd.DataFrame(ww_ts).interpolate(method='linear', limit_direction='forward', axis=1).to_numpy()
+ww_ts1 = pd.DataFrame(ww_ts).interpolate( limit_direction='both', axis=1).to_numpy()
 ww_tsm = pd.DataFrame(ww_ts1).rolling(14, axis=1).mean().to_numpy()
-data_4_s = smooth_epidata(data_4, 14, 0, 1)
+
+data_4_s = Smooth_epiData(data_4,smooth_factor = 14,week_correction = 0,week_smoothing = 1)
 data_diff = np.insert(np.diff(data_4_s, axis=1), 0, 0, axis=1)
 
 ww_tsw = np.insert(ww_ts[:, 5::7], 0, 0, axis=1)
 weekly_dat = np.insert(np.diff(ww_tsw, axis=1), 0, 0, axis=1)
 
+
+import csaps
 
 wlag = 7
 ww_tsm[ww_ts == 0] = np.nan
@@ -46,22 +89,37 @@ f[:, wlag:] = ww_ts[:, :-wlag] / data_diff[:, wlag:]
 f[:, :200] = np.nan
 f1 = f
 
-from scipy.interpolate import CubicSpline
 
 for jj in range(f1.shape[0]):
     xx = np.flatnonzero(~np.isnan(f1[jj,:]))
-    yy = f1[jj, xx]
-    if len(xx) > 5 and sum(xx > 200 and xx < 600) > 2:
-        f[jj, 200:] = CubicSpline(xx, yy, extrapolate=True)(range(200,f.shape[1]))
-    f[jj,:] = pd.DataFrame(f[jj,:]).interpolate(method='linear', limit_direction='forward', axis=1).to_numpy().ravel()
-    f[jj,:] = pd.DataFrame(f[jj,:]).interpolate(method='nearest', limit_direction='forward', axis=1).to_numpy().ravel()
+    xx2 = np.flatnonzero(np.isfinite(f1[jj,:]))
+    xx = np.array(list(set(xx2).intersection(set(xx))))
+    xx.sort()
+    
+    
+    if (len(xx) > 5) and (sum((xx>200) & (xx<600))) > 2:
+        yy = f1[jj, xx]
+        f[jj, 200:] = sp = csaps.CubicSmoothingSpline(xx, yy,smooth = 0.0001)(list(range(200,f.shape[1])))
+    f[jj,:] = pd.DataFrame(f[jj,:]).interpolate( limit_direction='both', axis=1).to_numpy().ravel()
+    f[jj,:] = pd.DataFrame(f[jj,:]).interpolate(limit_direction='both', axis=1).to_numpy().ravel()
 
 
 f[f<0] = 0
 f = pd.DataFrame(f).rolling(7,axis=1).mean().to_numpy()
 ww_adj = pd.DataFrame(data_diff*f).rolling(14,axis=1).mean().to_numpy()
 
-CDC_sero(). ### this function needs to be imported above
+from CDC_Sero import *
+CDC_SERO_Function()
+
+
+filehandler = open("true_new_infec.pkl", 'rb') 
+true_new_infec = pickle.load(filehandler)
+
+
+filehandler = open("un_array.pkl", 'rb') 
+un_array = pickle.load(filehandler)
+
+
 true_new_infec[0] = (true_new_infec[0]+abs(true_new_infec[0]))/2
 true_new_infec[1] = (true_new_infec[1]+abs(true_new_infec[1]))/2
 true_new_infec[2] = (true_new_infec[2]+abs(true_new_infec[2]))/2
@@ -70,41 +128,75 @@ true_new_infec[2] = (true_new_infec[2]+abs(true_new_infec[2]))/2
 eq_range = range(200,601)
 nz_idx = ww_adj[:, eq_range]>0.5
 
-ww_year1 = np.sum(ww_adj[:, eq_range]*nz_idx, axis=1)
+ww_year1 = np.nansum(ww_adj[:, eq_range]*nz_idx, axis=1)
 
-sero_year1 = np.sum(true_new_infec[1][:, eq_range]*nz_idx, axis=1)
-true_new_infec_ww[0] = (sero_year1/ww_year1)*ww_adj
-true_new_infec_ww[0] = pd.DataFrame(np.maximum(true_new_infec_ww[0], np.maximum(true_new_infec[0], un_array[:,0]*data_diff))).rolling(14,axis=1).mean().to_numpy()
 
-sero_year2 = np.sum(true_new_infec[1][:, eq_range]*nz_idx, axis=1)
-true_new_infec_ww[1] = (sero_year2/ww_year1)*ww_adj
-true_new_infec_ww[1] = pd.DataFrame(np.maximum(true_new_infec_ww[1], np.maximum(true_new_infec[1], un_array[:,1]*data_diff))).rolling(14,axis=1).mean().to_numpy()
+sero_year1 = np.nansum(true_new_infec[0].iloc[:, eq_range]*nz_idx, axis=1)
 
-sero_year3 = np.sum(true_new_infec[2][:, eq_range]*nz_idx, axis=1)
-true_new_infec_ww[2] = (sero_year3/ww_year1)*ww_adj
-true_new_infec_ww[2] = pd.DataFrame(np.maximum(true_new_infec_ww[2], np.maximum(true_new_infec[2], un_array[:,2]*data_diff))).rolling(14,axis=1).mean().to_numpy()
 
-# bad_states = (np.isnan(ww_ts).sum(axis=1) < 1) & (np.isnan(true_new_infec[1]).sum(axis=1) > 1)
+true_new_infec_ww = [0,0,0]
+b = sero_year1/ww_year1
+true_new_infec_ww[0] = ww_adj*b.reshape((b.size, 1))
+
+
+true_new_infec_ww[0][np.isnan(true_new_infec_ww[0])] = 0
+
+
+true_new_infec_ww[0] = pd.DataFrame(np.maximum(true_new_infec_ww[0],true_new_infec[0].to_numpy())).rolling(14,axis=1,min_periods=0).mean().to_numpy()
+
+
+eq_range = range(200,601)
+nz_idx = ww_adj[:, eq_range]>0.5
+
+# ww_year2 = np.nansum(ww_adj[:, eq_range]*nz_idx, axis=1)
+
+
+sero_year2 = np.nansum(true_new_infec[1].iloc[:, eq_range]*nz_idx, axis=1)
+
+b = sero_year2/ww_year1
+true_new_infec_ww[1] = ww_adj*b.reshape((b.size, 1))
+
+
+true_new_infec_ww[1][np.isnan(true_new_infec_ww[1])] = 0
+
+
+true_new_infec_ww[1] = pd.DataFrame(np.maximum(true_new_infec_ww[1],true_new_infec[1].to_numpy())).rolling(14,axis=1,min_periods=0).mean().to_numpy()
+
+
+eq_range = range(200,601)
+nz_idx = ww_adj[:, eq_range]>0.5
+
+# ww_year2 = np.nansum(ww_adj[:, eq_range]*nz_idx, axis=1)
+
+
+sero_year3 = np.nansum(true_new_infec[2].iloc[:, eq_range]*nz_idx, axis=1)
+
+b = sero_year3/ww_year1
+true_new_infec_ww[2] = ww_adj*b.reshape((b.size, 1))
+
+
+true_new_infec_ww[2][np.isnan(true_new_infec_ww[2])] = 0
+
+
+true_new_infec_ww[2] = pd.DataFrame(np.maximum(true_new_infec_ww[2],true_new_infec[2].to_numpy())).rolling(14,axis=1,min_periods=0).mean().to_numpy()
+
+
+bad_states = (np.isnan(ww_ts).sum(axis=1)<1) & (np.isnan(true_new_infec[1]).sum(axis=1)<1)
+
+
 # true_new_infec_ww[0][bad_states, :] = true_new_infec[0][bad_states, :]
 # true_new_infec_ww[1][bad_states, :] = true_new_infec[1][bad_states, :]
 # true_new_infec_ww[2][bad_states, :] = true_new_infec[2][bad_states, :]
 
-# bad_states = (np.isnan(ww_ts).sum(axis=1) < 1) & (np.isnan(true_new_infec[1]).sum(axis=1) < 1)
-# true_new_infec_ww[0][bad_states, :] = data_diff[bad_states, :]
-# true_new_infec_ww[1][bad_states, :] = data_diff[bad_states, :]
-# true_new_infec_ww[2][bad_states, :] = data_diff[bad_states, :]
-
-bad_states = (np.isnan(ww_ts).sum(axis=1)<1) & (np.isnan(true_new_infec[2]).sum(axis=1)>1)
-true_new_infec_ww[1][bad_states, :] = true_new_infec[1][bad_states, :]
-true_new_infec_ww[2][bad_states, :] = true_new_infec[2][bad_states, :]
-true_new_infec_ww[3][bad_states, :] = true_new_infec[3][bad_states, :]
-
-bad_states = (np.isnan(ww_ts).sum(axis=1)<1) & (np.isnan(true_new_infec[2]).sum(axis=1)<1)
+true_new_infec_ww[0][bad_states, :] = data_diff[bad_states, :]
 true_new_infec_ww[1][bad_states, :] = data_diff[bad_states, :]
 true_new_infec_ww[2][bad_states, :] = data_diff[bad_states, :]
-true_new_infec_ww[3][bad_states, :] = data_diff[bad_states, :]
+
+import pickle 
+with open("true_new_infec_ww.pkl", "wb") as f:
+    pickle.dump(true_new_infec_ww, f)
+with open("true_new_infec_final.pkl", "wb") as f:
+    pickle.dump(true_new_infec, f)
 
 
-
-
-
+print("Process completed")
